11e1f552f285232da7250def2926213eca08a21f
Koteuka-12 <lilianna.ewa.nowak@gmail.com>
2024-09-19 19:06:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+import random
+informacyje = ["Lubiem rysować, programować, trochę animować i robić specjalne błędy ortograficzne >:3", "Ni mam zwierzątka :(", "yyy, mój ulubiony projekt to taki z klikerem", "ogl, lubię duzo dań, ale moim ulu jest sushi"]
+print(random.choice(informacyje))
